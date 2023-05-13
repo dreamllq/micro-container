7495a45af5bd7def2b9474e9092d67006574a1ca
lvliqi <lvliqi_d@163.com>
2023-05-13 12:38:48 +0800

    feat: 调整目录结构

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -145,6 +145,7 @@ module.exports = {
     'vue/no-mutating-props': 'warn',
     'vue/require-valid-default-prop': 'warn',
     'vue/no-duplicate-attributes': 'warn',
-    '@typescript-eslint/no-non-null-assertion': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off'
   }
 };
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@
 import { createApp } from 'vue';
 import router from './router';
 import ElementPlus from 'element-plus';
+// @ts-ignore
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 import 'normalize.css';
-import './style.css';
+import '@/styles/style.scss';
 import App from '@/app';
+import 'dayjs/locale/zh-cn';
 
 const app = createApp(App);
 app.use(router);
